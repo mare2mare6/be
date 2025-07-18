7efa7d4925f118347b106b25f39a822cf65ec8f3
최하은 <sonagii.0.030@dsm.hs.kr>
2025-07-18 23:44:16 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char S[101];
+    int alphabet[26];
+
+    for (int i = 0; i < 26; i++) {
+        alphabet[i] = -1;
+    }
+
+    scanf("%s", S);
+
+    for (int i = 0; S[i] != '\0'; i++) {
+        int idx = S[i] - 'a';
+        if (alphabet[idx] == -1) { 
+            alphabet[idx] = i;
+        }
+    }
+
+    for (int i = 0; i < 26; i++) {
+        printf("%d ", alphabet[i]);
+    }
+
+    return 0;
+}
