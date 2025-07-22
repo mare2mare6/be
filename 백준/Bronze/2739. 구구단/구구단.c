348a446083ec5758f69c9c22b51988c7beacf958
최하은 <sonagii.0.030@dsm.hs.kr>
2025-07-22 22:41:04 +0900

    [Bronze V] Title: 구구단, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main() {
+
+        int A;
+
+        scanf("%d", &A);
+        printf("%d * 1 = %d\n", A, A * 1);
+        printf("%d * 2 = %d\n", A, A * 2);
+        printf("%d * 3 = %d\n", A, A * 3);
+        printf("%d * 4 = %d\n", A, A * 4);
+        printf("%d * 5 = %d\n", A, A * 5);
+        printf("%d * 6 = %d\n", A, A * 6);
+        printf("%d * 7 = %d\n", A, A * 7);
+        printf("%d * 8 = %d\n", A, A * 8);
+        printf("%d * 9 = %d\n", A, A * 9);
+
+        return 0;
+}
