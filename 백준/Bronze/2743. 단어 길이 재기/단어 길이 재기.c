0a2eda20b720bb728f2e2353c6e6e130ee5b8a20
최하은 <sonagii.0.030@dsm.hs.kr>
2025-07-23 23:11:03 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char word[101];
+    scanf("%s", word);
+    int length = strlen(word);
+    printf("%d\n", length);
+
+    return 0;
+}
