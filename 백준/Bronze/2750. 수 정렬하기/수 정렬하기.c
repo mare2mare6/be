d420240346a130ebe5cf883be95a5d0dc3c8ac5b
최하은 <sonagii.0.030@dsm.hs.kr>
2025-08-22 21:22:37 +0900

    [Bronze II] Title: 수 정렬하기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int compare(const void *a, const void *b) {
+    return (*(int*)a - *(int*)b);
+}
+
+int main() {
+    int N;
+    int arr[1000];
+    
+    scanf("%d", &N);
+    
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &arr[i]);
+    }
+    
+    qsort(arr, N, sizeof(int), compare);
+    
+    for (int i = 0; i < N; i++) {
+        printf("%d\n", arr[i]);
+    }
+    
+    return 0;
+}
