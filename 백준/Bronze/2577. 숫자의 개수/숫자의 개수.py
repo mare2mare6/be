1e3cb039fe7f63d049a8574fd60cb92a50e26b0f
최하은 <sonagii.0.030@dsm.hs.kr>
2025-12-11 18:56:04 +0900

    [Bronze II] Title: 숫자의 개수, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
@@ -2,8 +2,7 @@ A = int(input())
 B = int(input())
 C = int(input())
 
-result = A * B * C
-result_str = str(result)
+result = str(A * B * C)
 
 for i in range(10):
-    print(result_str.count(str(i)))
+    print(result.count(str(i)))
