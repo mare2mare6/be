aa289b23320b76ba39f0cfadaea1d4137a370d46
최하은 <sonagii.0.030@dsm.hs.kr>
2025-12-24 15:37:14 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.py
@@ -0,0 +1,12 @@
+i=input()
+
+if i=='M':
+    print("MatKor")
+elif i=='W':
+    print("WiCys")
+elif i=='C':
+    print("CyKor")
+elif i=='A':
+    print("AlKor")
+elif i=="$":
+    print("$clear")
